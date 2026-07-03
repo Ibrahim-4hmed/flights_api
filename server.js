8b4a9fd3723fca016bb508838497eb2ae7061b9b
Ibrahim-4hmed <dev.ibrahim.cs@gmail.com>
2026-07-03 05:47:34 +0200

    handle CROS logic

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ import {getDataFromDB} from './database/db.js'
 
 
 
-const PORT = process.env.PORT || 3000;;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer( async (req,res) => {
     const flightsData = await getDataFromDB();
 
     if (req.url === '/api' && req.method === 'GET') {
         res.setHeader('Content-Type', 'application/json')
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET')
         res.statusCode = 200
         res.end(JSON.stringify(flightsData))
     } else {
