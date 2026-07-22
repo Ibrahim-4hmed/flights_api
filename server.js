97cf7451a13ee8184323f1164c57f398a0607f51
Ibrahim-4hmed <dev.ibrahim.cs@gmail.com>
2026-07-22 05:02:46 +0200

    Update the code using Path and Fs modules

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 import http from 'node:http'
-import {getDataFromDB} from './database/db.js'
+// import {getDataFromDB} from './database/db.js'
 import { sendJSONResponse } from './utils/sendJSONResponse.js';
+import path from 'node:path';
+import fs from 'node:fs/promises'
 
 
 
 const PORT = process.env.PORT || 3000;
 
+const __dirname = import.meta.dirname
+
 const server = http.createServer( async (req,res) => {
-    const flightsData = await getDataFromDB();
-    
+
+const filePath = path.join(__dirname,'data','data.js')
+
 
     if (req.url === '/api' && req.method === 'GET') {
-        sendJSONResponse(res,200,flightsData)
+        try {
+            const flightsData = await  fs.readFile(filePath)
+            sendJSONResponse(res,200,flightsData)
+        } catch (error) {
+            console.log(error)
+        }
     } else {
         sendJSONResponse(res,404,
         {error: "not found", message: "The requested route does not exist"})
--- utils/sendJSONResponse.js
+++ utils/sendJSONResponse.js
@@ -3,5 +3,5 @@ export const sendJSONResponse = (res, statusCode, payload) => {
     res.setHeader('Access-Control-Allow-Origin', '*') // allow access from any domain
     res.setHeader('Access-Control-Allow-Methods', 'GET') // GET method only
     res.statusCode = statusCode
-    res.end(JSON.stringify(payload))
+    res.end(JSON.stringify(payload)) // to json string
 }
